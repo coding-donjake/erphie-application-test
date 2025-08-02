a55fc3ea7e62fe9dd784c88a1013c81a1adb3aab
coding-donjake <108913937+coding-donjake@users.noreply.github.com>
2025-08-02 15:11:26 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow, ipcMain, dialog } from "electron";
-import { join } from "path";
+import fs from "fs";
+import path, { join } from "path";
 
 const isDev = !app.isPackaged && process.env.NODE_ENV !== "production";
 
@@ -27,12 +28,64 @@ const createWindow = () => {
   }
 };
 
-ipcMain.handle("select-folder", () => {
-  const result = dialog.showOpenDialog({
-    properties: ["openDirectory"],
-  });
+ipcMain.handle("save-file", async (_event, defaultContent = "") => {
+  const { canceled, filePath } = (await dialog.showSaveDialog({
+    title: "Save file",
+    defaultPath: "untitled",
+  })) as any;
 
-  return result;
+  if (canceled || !filePath) {
+    return { success: false, canceled: true };
+  }
+
+  try {
+    fs.writeFileSync(filePath, defaultContent);
+    return { success: true, path: filePath };
+  } catch (error) {
+    return { success: false, error: (error as Error).message };
+  }
+});
+
+ipcMain.handle("save-folder", async (_event, folderName: string) => {
+  try {
+    const { canceled, filePaths } = (await dialog.showOpenDialog({
+      title: "Select location to create folder",
+      properties: ["openDirectory", "createDirectory"],
+    })) as any;
+
+    if (canceled || filePaths.length === 0) {
+      return { success: false, cancelled: true };
+    }
+
+    const selectedPath = filePaths[0];
+    const folderPath = path.join(selectedPath, folderName);
+
+    if (fs.existsSync(folderPath))
+      return { success: false, error: "Folder already exists." };
+
+    fs.mkdirSync(folderPath, { recursive: true });
+    return { success: true, path: folderPath };
+  } catch (error) {
+    return { success: false, error: (error as Error).message };
+  }
+});
+
+ipcMain.handle("select-file", async () => {
+  const { canceled, filePaths } = (await dialog.showOpenDialog({
+    title: "Select file",
+    properties: ["openFile"],
+  })) as any;
+
+  return canceled ? undefined : filePaths;
+});
+
+ipcMain.handle("select-folder", async () => {
+  const { canceled, filePaths } = (await dialog.showOpenDialog({
+    title: "Select folder",
+    properties: ["openDirectory"],
+  })) as any;
+
+  return canceled ? undefined : filePaths;
 });
 
 app.whenReady().then(createWindow);
